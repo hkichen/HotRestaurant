7a90e6b1873b6952207b3c27514deafd817d5569
Lisa Huynh <lisaahuynh@yahoo.com>
2018-05-05 13:01:48 -0400

    saved server.js logic for paths

--- server.js
+++ server.js
@@ -8,11 +8,104 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var customers = [
+// customer reservation & waitlist data
+var reservations = [
     {
-        customerName: [],
-        phoneNumber: [],
-        email: [],
-        customerid: []
+        customerName: "",
+        phoneNumber: "",
+        customerEmail: "",
+        customerId: ""
+    },
+    {
+        customerName: "",
+        phoneNumber: "",
+        customerEmail: "",
+        customerId: ""
+    },
+    {
+        customerName: "",
+        phoneNumber: "",
+        customerEmail: "",
+        customerId: ""
+    },
+    {
+        customerName: "",
+        phoneNumber: "",
+        customerEmail: "",
+        customerId: ""
+    },
+    {
+        customerName: "",
+        phoneNumber: "",
+        customerEmail: "",
+        customerId: ""
+    }
+
+];
+
+var waitlist = [
+    {
+        customerName: "",
+        phoneNumber: "",
+        customerEmail: "",
+        customerId: ""
+    },
+    {
+        customerName: "",
+        phoneNumber: "",
+        customerEmail: "",
+        customerId: ""
+    },
+    {
+        customerName: "",
+        phoneNumber: "",
+        customerEmail: "",
+        customerId: ""
+    },
+    {
+        customerName: "",
+        phoneNumber: "",
+        customerEmail: "",
+        customerId: ""
+    },
+    {
+        customerName: "",
+        phoneNumber: "",
+        customerEmail: "",
+        customerId: ""
     }
 ];
+
+// logic for button & passing input value to POST
+$('#reserveBtn').on("click", function (event) {
+    event.preventDefault();
+
+    var newReservation = {
+        customerName: $("#customerName").val().trim(),
+        phoneNumber: $("#phoneNumber").val().trim(),
+        customerEmail: $("#customerEmail").val().trim(),
+        customerId: $("#customerId").val().trim()
+    };
+})
+
+
+// routes 
+app.get("/", function(req, res) {
+    res.sendfile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendfile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendfile(path.join(__dirname, "reserve.html"));
+});
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
